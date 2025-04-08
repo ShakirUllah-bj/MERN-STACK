ca54f6f9640489a389134898713e04f4dcb6a178
Shakirullah_304 <shakir.edu304@gmail.com>
2025-04-08 12:41:17 -0700

    define js data types with example

---
+++ JavaScript/2-data-types/script.js
@@ -0,0 +1,37 @@
+// 1. String: Used to store text. Enclosed in quotes (' ', " ", ` `).
+// Example:
+let name = "Shakir Ullah";
+let address = 'Bajaur';
+
+// 2. Number: Stores numberic Values (both integers and decimals).
+// Example:
+let age = 20;
+let price = 50.4;
+
+// 3. Boolean: Only two values: true or false.
+//    Used for conditions.
+// Example:
+let isStudent = true;
+let isDoctor = false;
+
+// 4. Undefined: A variable that is declared but not given a value.
+// Example:
+let city;
+console.log(city) // Undefined
+
+// 5. Null: Represents no value or empty.
+// Example:
+let car = null;
+
+// 6. Object: Used to store multiple values in a single 
+//    variable (key-value pairs).
+// Example:
+let person = {
+    name: "Shakir",
+    address: "Bajaur",
+    age: 20
+}
+
+// 7. Array: Special type of object to store multiple values in a list.
+// Example:
+let fruits = ["apple", "banana", "orange", "mango"];
