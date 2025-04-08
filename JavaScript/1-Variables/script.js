ab56e37d99dd7685ab841bb08284eda97f59d07a
Shakirullah_304 <shakir.edu304@gmail.com>
2025-04-08 10:04:57 -0700

    define js variables with example

---
+++ JavaScript/1-Variables/script.js
@@ -0,0 +1,31 @@
+// What is Variable?
+// A Variable is a container for storing data values.
+// The data can be changed or used throughout the program.
+
+// How to Declare Variables:
+// We can declare variables in JavaScript using three keywords:
+
+// 1. var:
+// an older way of declaring variables, now considered less 
+// used due to issues like hoisting.
+
+// Example: 
+// var myName = "Shakir Ullah";
+// var myAge = 20;
+// var isStudent = true;
+
+// 2. let:
+//    A modern way to declare variables with block Scope.
+
+//Example:
+//let myName = "Shakir Ullah";
+//let myAge = 20;
+//let isStudent = true;
+
+// 3. const:
+//    used to declare variables that cannot be reassigned (constant values).
+
+//Example:
+//const myName = "Shakir Ullah";
+//const birthYear = 2004;
+//const isMale = true;
