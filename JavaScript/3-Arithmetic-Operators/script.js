3f4ac062c312f41e28ed14d45960e6795d5fe9b6
Shakirullah_304 <shakir.edu304@gmail.com>
2025-04-08 13:12:20 -0700

    define js arithmetic and acreament/decrement operator with examples

---
+++ JavaScript/3-Arithmetic-Operators/script.js
@@ -0,0 +1,52 @@
+// Aritmetic Operators:
+// Arithmetic Operators are used to perform basic math operations
+
+
+// Concatination Operator (+):
+// Used to join two or more strings.
+// Exmaple:
+// "hello" + "word" = "hello world"
+
+// 1. + (Addition): Adds two numbers.
+// Example:
+// let num1 = 20, num2 = 5;
+// let result = num1 + num2;
+// console.log(result);
+
+// 2. - (Subtraction): Subracts one number from another
+// Example
+
+// let num1 = 10, num2 = 5;
+// let result = num1 -  num2;
+// console.log(result);
+
+
+// 3. * (Multiplication): Multiplies two numbers.
+// Example:
+// let num1 = 5, num2 = 8;
+// let result = num1 * num2;
+// console.log(result);
+
+// 4. / (Divsion): Devides one number by another.
+// Exapmle:
+// let num1 = 50, num2 = 10;
+// let result = num1 / num2;
+// console.log(result);
+
+// 5. % (Modulus): Gives the remainder after division.
+// Example:
+// let num1 = 9, num2 = 2;
+// let result = num1 % num2;
+// console.log(result);
+
+// 6. ++ (Increament): Increases a number by 1.
+// Example:
+// let number = 10;
+// number++;
+// console.log(number) // 11
+
+// 7. -- (Decrement): Decreases a number by 1.
+// Example:
+// let number = 10;
+// number--;
+// console.log(number) // 9
