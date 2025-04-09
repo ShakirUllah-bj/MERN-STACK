f6c22d8bd87ae84686f028c0dfc023a9a5ce67ca
Shakirullah_304 <shakir.edu304@gmail.com>
2025-04-09 02:04:07 -0700

    define js assignment operators with examples

---
+++ JavaScript/4-Assignment-Operators/script.js
@@ -0,0 +1,67 @@
+// What is JavaScript Assignment Operators?
+// Assignment operators store values in variables.
+// They can also perform calculations (like add, subtract, multiply) and then store the result in the same variable.
+
+// 1. = (Simple Assignment Operator):
+// This is used to assign a value to a variable.
+
+// Example:
+// let x = 10;  // 10 is stored in x
+// console.log(x);
+
+// 2. += (Add and Assign):
+// This adds a value to the variable and then stores the result back in the same variable.
+
+// Example:
+// let x = 10;
+// x += 5;  // same as x = x + 5 -> x becomes 15
+// console.log(x);
+
+// 3. -= (Subtract and Assign):
+// This subtract a value from the variable and then stores the result back.
+
+// Example:
+// let x = 20;
+// x -= 5; // same as x = x - 5 -> x becomes 15
+// console.log(x);
+
+
+// 4. *= (Multiply and Assign):
+// This multiplies the variable with a value or another variable and store
+// the result back in the variable.
+
+// Example 1:
+// let x = 10;
+// x *= 5; // same as x = x * 5 -> x becomes 50
+// console.log(x);
+
+// Example 2:
+// let num1 = 20, num2 = 5;
+// num1 *= num2;
+// console.log(num1) // same as num1 = num1 * num2 -> num1 becomes 100
+
+// 5. /= (Divide and Assign):
+// This divide a variable by a value or another variable and
+// store the result back in the same variable.
+
+// Example:
+// let num = 10;
+// num /= 3; // same num = num / 3 -> num becomes 3.3333333333333335
+// console.log(num);
+
+// 5. %= (Modulus and Assign):
+// This finds the remainder when the variable is divided by
+// a value or another variable and store that remainder.
+
+// Example: 
+// let x = 10;
+// x %= 3; // same is x = x % 3 -> x becomes 1
+// console.log(x);
+
+// 7. **= (Exponentiation and Assign):
+// This raises the variable to the power of a value and store the result. 
+
+// Example:
+// let x = 5;
+// x **= 3; // same as x = x **= 3 -> x becomes 125
+// console.log(x);
