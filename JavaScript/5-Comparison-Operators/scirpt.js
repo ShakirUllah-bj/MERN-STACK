89a7cc9040b695a1384117aa41b7bfb2ddc480ed
Shakirullah_304 <shakir.edu304@gmail.com>
2025-04-09 13:23:24 -0700

    define js comparison operators with examples

---
+++ JavaScript/5-Comparison-Operators/scirpt.js
@@ -0,0 +1,53 @@
+// // What is Comparison Operators in JavaScript ?
+// // Comparison operators in JavaScript are used to compare
+// // two values and return true or false based on the comparison.
+
+// 1. == (Equal to):
+// Checks if two values are equal, but does not check the data type.
+// Example:
+// console.log(5 == '5') // -> true
+    
+// 2. === (Strict equal to):
+// Checks if values are equal and of the same type.
+// Example:
+// console.log(5 === '5') // -> false
+// console.log(5 === 5) //  -> true
+
+// 3. != (Not equal to):
+// Checks if two values are not equal, but ignores data type.
+// Example:
+// console.log(5 != 3) // -> true
+// console.log(5 != 5) // -> false
+
+// 4. !== (Strict not equal to):
+// Checks if two values are not equal or not the same type.
+// Example:
+// console.log(5 !== '5') // -> true
+// console.log(5 !== 5) // -> false
+
+// 5. > (Greater than):
+// Checks if the left value is greater than the right one.
+// Example:
+// console.log(7 > 5) // -> true
+// console.log(5 > 7) // -> false
+
+// 6. < (Less than):
+//  Check if the left value is less than the right one.
+//  Example:
+//  console.log(7 < 5) // -> false
+//  console.log(5 < 7) // -> true
+ 
+// 7. >= (Greater than or equal to):
+//  Checks if the left values is greater than
+//  or equal to the right one.
+//  Example:
+//  console.log(5 >= 5) // -> true
+//  console.log(5 >= 4) // -> true
+ 
+// 8. <= (Less than or equal to):
+//  Checks if the left value is less than or equal to the righ one.
+//  Example:
+//  console.log(5 <= 5) // -> true
+//  console.log(4 <= 5) // -> true
+
+ 
